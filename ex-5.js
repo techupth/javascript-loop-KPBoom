0adc9018d52fa2c98b015029270baabeb9eeed28
Boom <boom2k31@gmail.com>
2024-04-06 16:42:36 +0700

    Finish Assign

--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,8 @@
 let companyName = "TechUp";
 
 // Start coding here
+let n=1;
+for ( let i of companyName){
+    console.log("Number "+n+" character is "+i);
+    n++;
+}
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,8 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let i of companyName){
+    reversedCompanyName = i + reversedCompanyName;
+}
 
 console.log(reversedCompanyName);
